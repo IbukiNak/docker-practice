88f8a732591e3c3e844d167f4fe0a5bc093ccb76
NakamuraIbuki <i.nakamura.sys23@morijyobi.ac.jp>
2024-06-28 10:25:07 +0900

    todo_app

---
+++ todo_app/config/urls.py
@@ -0,0 +1,8 @@
+# config/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("mytodo.urls")), # mytodo/urls.pyを参照
+]
